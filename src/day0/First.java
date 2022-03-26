2f43bd849ed9b20b276bfa011ef128a9e0db3c16
Maiwand Alikhail <maiwand_alikhail@hotmail.com>
2022-03-26 16:10:22 -0400

    updated and added code

--- src/day0/First.java
+++ src/day0/First.java
@@ -1,8 +1,11 @@
 package day0;
 
 public class First {
-    //TODO works on this tomorrow
-    // add some cool code
+    public static void main(String[] args) {
+        System.out.println("java");
+        System.out.println("working");
+        System.out.println("on git");
+    }
 
     //extra line 1
     //extra line 1
